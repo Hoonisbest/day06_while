a898cfdfc8efb202286a9714367b9edf0f4a6a3d
Hoon <dkhj0615@naver.com>
2021-07-30 15:04:35 +0900

    팀퀴즈까지 완료

--- src/day06_while/Myquiz.java
+++ src/day06_while/Myquiz.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Myquiz {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		
+		//완전수 프로그램
+		Scanner input = new Scanner(System.in);
 
         System.out.print("수 입력 : ");
-        int n = sc.nextInt();
+        int x = input.nextInt();
         
         
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<=x; i++) {
         	int sum = 0;
             for(int j=1; j<i; j++) {
                 if(i%j==0) {
@@ -21,9 +23,64 @@ public class Myquiz {
                 System.out.println(i);
             }
         }
+        	
+        //별 찍기 프로그램
+        
+        for (int i=0; i<5; i++) {
+			for (int j=0; j<5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+        //별 피라미드
+		for( i = 0; i < 6; i++){
 
-    }
+			for(int j = 15; j > i; j--){
 
-}
+			System.out.print(" ");
+
+			}
+
+			for(int k = 0; k < i * 2 - 1; k++){
+
+			System.out.print("*");
+
+			}
+
+			System.out.println();
+
+			}
+		//시간 출력 
+		for (int h = 0; h <= 23; h++) {
+			for (int m = 0; m <= 59; m++) {
+				  System.out.println(h + "시" + m + "분" );
+				 
+				}
+			}
+		
+		//2중포문 별출력
+		for ( i =1;i<=10;i++) {
+			for(int j =1;j<=10;j++) {
+				System.out.println("*");
+			}
+		}
+			
+
+
+
+		
+		
+
+		}
+
+
+
+
+        
+        } 
+	}   
+        
+    
+
+
 
 
--- src/day06_while/Quiz01.java
+++ src/day06_while/Quiz01.java
@@ -29,17 +29,6 @@ public class Quiz01 {
 		}i--;
 		System.out.println(i+"  : "+sum);
 		
-		
-		i = 1;
-		sum = 0;
-	      
-		for(; sum < 10000;i++) {
-		   if(i%2 == 1) {
-		   sum += i;
-		   }
-		}
-		i--;
-		System.out.println(i + " : " + sum);
 
 		
 		
